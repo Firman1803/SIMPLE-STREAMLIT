ffa3c89210195bd867e64db82b473df61adf64a7
Firman1803 <firmanhidayahtullah@students.unnes.ac.id>
2025-05-22 09:06:18 +0700

    Create 1_PROJECT.py

---
+++ pages/1_PROJECT.py
@@ -0,0 +1,8 @@
+import streamlit as st
+impiort pandas as pd
+import joblib
+from sklearn.model_selection import train_test_spilt
+form sklearn.metrics import classification_reprt, accuracy_score
+
+st.set_page_config(page_title="selamat")
+st.header("selamat")
