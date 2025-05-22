f7a9822934e756ba39a127cc5cce30507e8035e2
Firman1803 <firmanhidayahtullah@students.unnes.ac.id>
2025-05-22 09:10:32 +0700

    Update 2_PROJECT.py

--- pages/2_PROJECT.py
+++ pages/2_PROJECT.py
@@ -1,9 +1,6 @@
 import streamlit as st
-impiort pandas as pd
-import joblib
-from sklearn.model_selection import train_test_spilt
-form sklearn.metrics import classification_reprt, accuracy_score
 
-st.set_page_config(page_title="BOYS SCOUT")
+
+st.set_page_config(page_title="BOYS SCOUT", layout="centered")
 st.title("BOYS SCOUT")
 
